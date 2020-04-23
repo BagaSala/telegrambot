310b15422e48edc1e9537c2d6caafedfedef7259
BagaSala <bodypronin.2121@gmail.com>
2020-04-23 12:25:44 +0600

    setting db config

--- src/main/java/com/github/bagasala/DatabaseConfiguration.java
+++ src/main/java/com/github/bagasala/DatabaseConfiguration.java
@@ -1,7 +1,19 @@
 package com.github.bagasala;
 
+import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+
+import java.sql.SQLException;
 
 public class DatabaseConfiguration {
     private static final String DB_URL = "jdbc:sqlite:/Users/bogdan/Desktop/mncDB.db";
+    public static ConnectionSource source;
+
+    static {
+        try {
+            source = new JdbcPooledConnectionSource(DB_URL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
