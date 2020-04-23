1a6e6325f89196cc4a083a74fffbfdd8fd0fc867
BagaSala <bodypronin.2121@gmail.com>
2020-04-23 12:21:30 +0600

    first commit

---
+++ src/main/java/com/github/bagasala/DatabaseConfiguration.java
@@ -0,0 +1,7 @@
+package com.github.bagasala;
+
+import com.j256.ormlite.support.ConnectionSource;
+
+public class DatabaseConfiguration {
+    private static final String DB_URL = "jdbc:sqlite:/Users/bogdan/Desktop/mncDB.db";
+}
